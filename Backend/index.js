78fcdb03ec3b89c1e564e876fca07491ad74e710
Gunjan <gunjanaggarwal278@gmail.com>
2025-06-11 12:55:23 +0530

    Updated

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ import authRoute from "./Routes/authRoute.js";
 import verifyJWT from "./Middlewares/verifyJWT.js";
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://invoiceease-3.onrender.com', 
   credentials: true               
 }));
 app.use(express.json());
