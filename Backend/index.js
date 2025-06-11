d045ff1c4852f573957dc3fe533da17a13dcd454
Gunjan <gunjanaggarwal278@gmail.com>
2025-06-11 12:17:03 +0530

    made changes

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser())
-const PORT = 5000;
+const PORT = process.env.PORT|| 5000;
 // const PORT = process.env.PORT||5000;
 
 // Minimal test route
